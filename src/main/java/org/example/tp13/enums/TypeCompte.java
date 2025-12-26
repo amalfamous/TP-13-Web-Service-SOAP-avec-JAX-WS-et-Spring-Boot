2172c93db6d67add3901d31c010b8c751534913c
amal <amalfms@gmail.com>
2025-12-26 22:45:34 +0100

    first commit

---
+++ src/main/java/org/example/tp13/enums/TypeCompte.java
@@ -0,0 +1,7 @@
+package org.example.tp13.enums;
+
+
+public enum TypeCompte {
+    COURANT,
+    EPARGNE
+}
---
+++ src/main/java/org/example/tp13/repositories/CompteRepository.java
@@ -0,0 +1,8 @@
+package org.example.tp13.repositories;
+
+import org.example.tp13.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {}
